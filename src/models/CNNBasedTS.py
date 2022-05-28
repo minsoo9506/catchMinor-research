0b41088b918ff4962fea1753021c62d8e0a99506
minsoo9506 <ghktjd15gh@gmail.com>
2022-05-28 15:26:49 +0900

    feat: implement CNN (U-Net) based time series anomaly detection model

--- src/models/CNNBasedTS.py
+++ src/models/CNNBasedTS.py
@@ -50,7 +50,7 @@ def CBR1d(
 class TSUNet(nn.Module):
     def __init__(
         self,
-        feature_dim: int
+        feature_dim: int = 1 
         ):
         super().__init__()
 
@@ -71,11 +71,71 @@ class TSUNet(nn.Module):
         self.enc4_2 = CBR1d(in_channels=128, out_channels=128)
         self.pool4 = nn.MaxPool1d(kernel_size=4)
 
-        self.enc5_1 = CBR1d(in_channels=128, out_channels=256)
-        self.enc5_2 = CBR1d(in_channels=256, out_channels=256)
-        self.pool5 = nn.MaxPool1d(kernel_size=4)
-
         # decode
+        self.dec1_1 = CBR1d(in_channels=128, out_channels=256)
+        self.dec1_2 = CBR1d(in_channels=256, out_channels=256)
+        self.upsample1 = nn.ConvTranspose1d(kernel_size=4)
+
+        self.dec2_1 = CBR1d(in_channels=256, out_channels=128)
+        self.dec2_2 = CBR1d(in_channels=128, out_channels=128)
+        self.upsample2 = nn.ConvTranspose1d(kernel_size=4)
+
+        self.dec3_1 = CBR1d(in_channels=128, out_channels=64)
+        self.dec3_2 = CBR1d(in_channels=64, out_channels=64)
+        self.upsample3 = nn.ConvTranspose1d(kernel_size=4)
+
+        self.dec4_1 = CBR1d(in_channels=64, out_channels=32)
+        self.dec4_2 = CBR1d(in_channels=32, out_channels=32)
+        self.upsample4 = nn.ConvTranspose1d(kernel_size=4)
+
+        self.dec5_1 = CBR1d(in_channels=32, out_channels=16)
+        self.dec5_2 = CBR1d(in_channels=16, out_channels=16)
+        
+        self.last = nn.Conv1d(in_channels=16, out_channels=feature_dim, kernel_size=3, stride=1, padding=1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pass
+        # encode
+        enc1_1 = self.enc1_1(x)
+        enc1_2 = self.enc1_2(enc1_1)
+        pool1 = self.pool1(enc1_2)
+
+        enc2_1 = self.enc2_1(pool1)
+        enc2_2 = self.enc1_2(enc2_1)
+        pool2 = self.pool2(enc2_2)
+
+        enc3_1 = self.enc3_1(pool2)
+        enc3_2 = self.enc3_2(enc3_1)
+        pool3 = self.pool3(enc3_2)
+
+        enc4_1 = self.enc4_1(pool3)
+        enc4_2 = self.enc4_2(enc4_1)
+        pool4 = self.pool4(enc4_2)
+
+        # decode
+        dec1_1 = self.dec1_1(pool4)
+        dec1_2 = self.dec1_2(dec1_1)
+        upsample1 = self.upsample1(dec1_2)
+
+        concat1 = torch.concat((enc4_2, upsample1), dim=1) # dim = 1 : channel dim에 합치는 것        
+        dec2_1 = self.dec2_1(concat1)
+        dec2_2 = self.dec2_2(dec2_1)
+        upsample2 = self.upsample2(dec2_2)
+        
+        concat2 = torch.concat((enc3_2, upsample2), dim=1)
+        dec3_1 = self.dec3_1(concat2)
+        dec3_2 = self.dec3_2(dec3_1)
+        upsample3 = self.upsample3(dec3_2)
+
+        concat3 = torch.concat((enc2_2, upsample3), dim=1)
+        dec4_1 = self.dec4_1(concat3)
+        dec4_2 = self.dec4_2(dec4_1)
+        upsample4 = self.upsample4(dec4_2)
+
+        dec5_1 = self.dec5_1(upsample4)
+        dec5_2 = self.dec5_2(dec5_1)
+        
+        last = self.last(dec5_2)
+        output = self.sigmoid(last)
+        
+        return output
